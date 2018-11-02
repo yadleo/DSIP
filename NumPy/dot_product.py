60b5f6d84ce30e2143d87b132b29adf5c1a53662
Daniel Lai <dlai813@gmail.com>
2018-11-02 14:40:49 -0700

    Completed dot_product

---
+++ NumPy/dot_product.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+def dot_product(a, b):
+  '''
+  Return the dot product of two numpy arrays of the same length.
+
+  Parameters
+  ----------
+  a: numpy array with shape (n, )
+      An array of floating point numbers.
+  b: numpy array with shape (n, )
+      An array of floating point numbers.
+
+  Returns
+  -------
+  dot_prod: float
+      The dot product of a and b.
+  '''
+  return np.sum(a * b)
+
+dot_product([1,2,3],[5,6,7])
